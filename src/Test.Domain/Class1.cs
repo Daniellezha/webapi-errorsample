24012f5472ac39cfa2ddd81615f2243ac9ca0ba4
LRPalacios <lrpalaciosdev@gmail.com>
2016-09-23 13:52:25 -0600

    initial commit

---
+++ src/Test.Domain/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Domain
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
